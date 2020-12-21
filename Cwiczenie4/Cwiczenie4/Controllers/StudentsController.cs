46ecbeebcdb90af808c7a369ad6663978297096c
Weronika Jaworek <s18630@pjwstk.edu.pl>
2020-12-21 19:27:40 +0100

    Utworzona jedna metoda w kontlorerze zwracająca listę studentów

---
+++ Cwiczenie4/Cwiczenie4/Controllers/StudentsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Cwiczenie4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cwiczenie4.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        private const string ConString = "Data Source=db-mssql;Initial Catalog=2019SBD;Integrated Security=True";
+
+        [HttpGet]
+        public IActionResult GetStudent()
+        {
+            var list = new List<Student>();
+            using (SqlConnection con = new SqlConnection(ConString))
+            using(SqlCommand com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "select * from Student";
+
+                con.Open();
+                SqlDataReader dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    var st = new Student();
+                    st.IndexNumber = dr["IndexNumber"].ToString();
+                    st.FirstName = dr["FirstName"].ToString();
+                    st.LastName = dr["LastName"].ToString();
+                    st.BirthDate = dr["BirthDate"].ToString();
+                    st.IdEnrollment = dr["IdEnrollment"].ToString();
+
+                    list.Add(st);
+
+
+                }
+                    
+            }
+            return Ok(list);
+        }
+    }
+}
